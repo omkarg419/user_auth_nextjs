44de6de3810be877611e14c77f5cc8a68ea59fbf
omkarg419 <68603231+omkarg419@users.noreply.github.com>
2025-12-13 13:59:43 +0530

    feat: implement login and signup pages with MongoDB connection

---
+++ src/app/login/page.tsx
@@ -0,0 +1,7 @@
+export default function LoginPage() {
+  return (
+    <div className="flex justify-center items-center">
+      <h1 className="text-2xl font-bold text-center align-middle m-2.5 p-2">Login Page</h1>
+    </div>
+  );
+}
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,9 @@
+export default function SignupPage() {
+  return (
+    <div className="flex justify-center items-center">
+      <h1 className="text-2xl font-bold text-center align-middle m-2.5 p-2">
+        Signup Page
+      </h1>
+    </div>
+  );
+}
