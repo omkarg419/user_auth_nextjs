cf27e0afd6bef5d07f02d33c40eaebd6a02604f0
omkarg419 <68603231+omkarg419@users.noreply.github.com>
2025-12-15 15:18:36 +0530

    feat: enhance login and signup pages with improved UI and user input handling

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,54 @@
+"use client";
+import React from "react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { axios } from "axios";
+
 export default function LoginPage() {
+  const [user, setUser] = React.useState({
+    email: "",
+    password: "",
+  });
+
+  const onLogin = async () => {};
+
   return (
-    <div className="flex justify-center items-center">
-      <h1 className="text-2xl font-bold text-center align-middle m-2.5 p-2">Login Page</h1>
+    <div className="flex flex-col justify-center items-center min-h-screen  py-2">
+      <div className=" flex flex-col justify-center items-center py-2 rounded-2xl p-10 bg-transparent opacity-100 space-y-4 shadow-amber-600  shadow-2xl ">
+        <h1 className="text-2xl text-amber-500 font-bold text-center align-middle m-2.5 p-2">
+          Login
+        </h1>
+        <hr />
+
+        <input
+          className="p-2 text-black border-none  bg-amber-200 rounded-2xl w-70 mt-4"
+          type="email"
+          id="email"
+          value={user.email}
+          onChange={(e) => setUser({ ...user, email: e.target.value })}
+          placeholder="Enter Email"
+        />
+        <input
+          className="p-2 text-black border-none  bg-amber-200 rounded-2xl w-70 mt-4"
+          type="password"
+          id="password"
+          value={user.password}
+          onChange={(e) => setUser({ ...user, password: e.target.value })}
+          placeholder="Enter Password"
+        />
+        <button
+          className="p-2 bg-amber-500 text-white rounded-lg mt-4 w-70"
+          onClick={onLogin}
+        >
+          Login here
+        </button>
+        <Link
+          href="/signup"
+          className="text-blue-600 hover:text-blue-800 hover:underline "
+        >
+          Don't have an account? Signup here
+        </Link>
+      </div>
     </div>
   );
 }
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -15,35 +15,49 @@ export default function SignupPage() {
 
   return (
     <div className="flex flex-col justify-center items-center min-h-screen  py-2">
-      <h1 className="text-2xl text-amber-100 font-bold text-center align-middle m-2.5 p-2">
-        Signup
-      </h1>
-      <hr />
+      <div className=" flex flex-col justify-center items-center py-2 rounded-2xl p-10 bg-transparent opacity-100 space-y-4 shadow-amber-600  shadow-2xl ">
+        <h1 className="text-2xl text-amber-500 font-bold text-center align-middle m-2.5 p-2">
+          Signup
+        </h1>
+        <hr />
 
-      <input
-        className="p-2 text-black border-none  bg-amber-200 rounded-2xl w-70"
-        type="text"
-        id="username"
-        value={user.username}
-        onChange={(e) => setUser({ ...user, username: e.target.value })}
-        placeholder="Enter Username"
-      />
-      <input
-        className="p-2 text-black border-none  bg-amber-200 rounded-2xl w-70 mt-4"
-        type="email"
-        id="email"
-        value={user.email}
-        onChange={(e) => setUser({ ...user, email: e.target.value })}
-        placeholder="Enter Email"
-      />
-      <input
-        className="p-2 text-black border-none  bg-amber-200 rounded-2xl w-70 mt-4"
-        type="password"
-        id="password"
-        value={user.password}
-        onChange={(e) => setUser({ ...user, password: e.target.value })}
-        placeholder="Enter Password"
-      />
+        <input
+          className="p-2 text-black focus:ring-2 focus:ring-amber-700 bg-amber-200 rounded-2xl w-70"
+          type="text"
+          id="username"
+          value={user.username}
+          onChange={(e) => setUser({ ...user, username: e.target.value })}
+          placeholder="Enter Username"
+        />
+        <input
+          className="p-2 text-black border-none  bg-amber-200 rounded-2xl w-70 mt-4"
+          type="email"
+          id="email"
+          value={user.email}
+          onChange={(e) => setUser({ ...user, email: e.target.value })}
+          placeholder="Enter Email"
+        />
+        <input
+          className="p-2 text-black border-none  bg-amber-200 rounded-2xl w-70 mt-4"
+          type="password"
+          id="password"
+          value={user.password}
+          onChange={(e) => setUser({ ...user, password: e.target.value })}
+          placeholder="Enter Password"
+        />
+        <button
+          className="p-2 bg-amber-500 text-white rounded-lg mt-4 w-70"
+          onClick={onSignup}
+        >
+          Signup here
+        </button>
+        <Link
+          href="/login"
+          className="text-blue-600 hover:text-blue-800 hover:underline "
+        >
+          Already have an account? Login here
+        </Link>
+      </div>
     </div>
   );
 }
