08f240bf04724001ee0a07eee216436b1478a630
omkarg419 <68603231+omkarg419@users.noreply.github.com>
2025-12-24 16:17:18 +0530

    feat: enhance login functionality with loading state and button disable logic

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,22 +1,45 @@
 "use client";
-import React from "react";
+import React, { useEffect } from "react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import axios from "axios";
 
 export default function LoginPage() {
+  const router = useRouter();
   const [user, setUser] = React.useState({
     email: "",
     password: "",
   });
 
-  const onLogin = async () => {};
+  const [buttonDisabled, setButtonDisabled] = React.useState(false);
+  const [loading, setLoading] = React.useState(false);
+
+  const onLogin = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.post("/api/users/login", user);
+      console.log("Login successful",response.data);
+      router.push("/profile");
+    } catch (error: any) {
+      console.log("Login failed", error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    if (user.email.length > 0 && user.password.length > 0) {
+      setButtonDisabled(false);
+    } else {
+      setButtonDisabled(true);
+    }
+  }, [user]);
 
   return (
     <div className="flex flex-col justify-center items-center min-h-screen  py-2">
       <div className=" flex flex-col justify-center items-center py-2 rounded-2xl p-10 bg-transparent opacity-100 space-y-4 shadow-amber-600  shadow-2xl ">
         <h1 className="text-2xl text-amber-500 font-bold text-center align-middle m-2.5 p-2">
-          Login
+          { loading ? "Processing..." : "Login"}
         </h1>
         <hr />
 
