34818cfe0770e008d171bd8113467b15cc8fc5da
omkarg419 <68603231+omkarg419@users.noreply.github.com>
2025-12-24 15:37:31 +0530

    feat: add JWT token creation and cookie handling in login route

--- src/app/api/users/login/route.ts
+++ src/app/api/users/login/route.ts
@@ -2,12 +2,12 @@ import { connectDB } from "@/dbconfig/dbconfig";
 import User from "@/models/userModel";
 import bcrypt from "bcryptjs";
 import { NextResponse, NextRequest } from "next/server";
+import jwt from "jsonwebtoken";
 
 connectDB();
 
 export async function POST(request: NextRequest) {
   try {
-
     const reqBody = await request.json();
     const { email, password } = reqBody;
     console.log(reqBody);
@@ -30,6 +30,31 @@ export async function POST(request: NextRequest) {
       );
     }
 
+    // create token data
+    const tokenData = {
+      id: user._id,
+      username: user.username,
+      email: user.email,
+    };
+
+    // create a JWT token
+    const token = await jwt.sign(tokenData, process.env.TOKEN_SECRET!, {
+      expiresIn: "1d",
+    });
+
+    const response = NextResponse.json({
+      message: "Login successful",
+      success: true,
+    });
+
+    // set token in httpOnly cookie
+    response.cookies.set("token", token, {
+      httpOnly: true,
+      //   secure: process.env.NODE_ENV === "production",
+      //   maxAge: 24 * 60 * 60, // 1 day
+    });
+
+    return response;
   } catch (error) {
     return NextResponse.json(
       { error: "Internal Server Error" },
