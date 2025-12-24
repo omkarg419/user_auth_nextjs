91a3e83c9b066a457bab00f68515b0127c2bdfcc
omkarg419 <68603231+omkarg419@users.noreply.github.com>
2025-12-24 14:17:55 +0530

    feat: implement login functionality with user authentication and error handling

--- src/app/api/users/login/route.ts
+++ src/app/api/users/login/route.ts
@@ -3,3 +3,37 @@ import User from "@/models/userModel";
 import bcrypt from "bcryptjs";
 import { NextResponse, NextRequest } from "next/server";
 
+connectDB();
+
+export async function POST(request: NextRequest) {
+  try {
+
+    const reqBody = await request.json();
+    const { email, password } = reqBody;
+    console.log(reqBody);
+
+    // Check if user exists
+    const user = await User.findOne({ email });
+    if (!user) {
+      return NextResponse.json(
+        { error: "Invalid email or password" },
+        { status: 401 }
+      );
+    }
+
+    // Check if password is correct
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return NextResponse.json(
+        { error: "Invalid email or password" },
+        { status: 401 }
+      );
+    }
+
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
