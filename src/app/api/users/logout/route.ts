ba74d11071fad51b5bddc142fdbed740a6065896
omkarg419 <68603231+omkarg419@users.noreply.github.com>
2025-12-26 15:26:32 +0530

    feat: implement logout functionality with token cookie clearance

--- src/app/api/users/logout/route.ts
+++ src/app/api/users/logout/route.ts
@@ -3,7 +3,18 @@ import { NextResponse } from "next/server";
 export async function POST() {
   try {
 
+const response = NextResponse.json(
+    {
+        message: "Logout successful",
+        success: true,
+    }
+)
 
+    // Clear the token cookie
+    response.cookies.set("token", "", {
+      httpOnly: true,
+      maxAge: 0, // Expire the cookie immediately
+    });
 
   } catch (error: any) {
     return NextResponse.json(
