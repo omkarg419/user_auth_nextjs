3674d7fe8e34cf420ace953e41fd61432ca3b9c6
omkarg419 <68603231+omkarg419@users.noreply.github.com>
2025-12-26 16:01:00 +0530

    feat: add logout button to profile page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,12 +1,23 @@
 "use client";
+import axios from "axios";
+import Link from "next/link";
+
 export default function ProfilePage() {
   return (
-    <div className="flex flex-col justify-center items-center min-h-screen  py-2">       
-        <div className=" flex flex-col justify-center items-center py-2 rounded-2xl p-10 bg-transparent opacity-100 space-y-4 shadow-amber-600  shadow-2xl ">
-            <h1 className="text-2xl text-amber-500 font-bold text-center align-middle m-2.5 p-2">Profile</h1>
-            <hr />
-            <p className="text-amber-300">Welcome to your profile page!</p>
-        </div>
+    <div className="flex flex-col justify-center items-center min-h-screen  py-2">
+      <div className=" flex flex-col justify-center items-center py-2 rounded-2xl p-10 bg-transparent opacity-100 space-y-4 shadow-amber-600  shadow-2xl ">
+        <h1 className="text-2xl text-amber-500 font-bold text-center align-middle m-2.5 p-2">
+          Profile
+        </h1>
+        <hr />
+        <p className="text-amber-300">Welcome to your profile page!</p>
+      </div>
+
+      <div className="mt-10">
+        <button className="bg-amber-500 hover:bg-amber-600 text-white font-bold py-2 px-4 rounded">
+          Logout
+        </button>
+      </div>
     </div>
   );
 }
