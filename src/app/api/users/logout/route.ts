33fddeddc17e53564aed3674de0ccb29ab483ee0
omkarg419 <68603231+omkarg419@users.noreply.github.com>
2025-12-26 15:30:46 +0530

    logout logic implemented

--- src/app/api/users/logout/route.ts
+++ src/app/api/users/logout/route.ts
@@ -2,20 +2,17 @@ import { NextResponse } from "next/server";
 
 export async function POST() {
   try {
-
-const response = NextResponse.json(
-    {
-        message: "Logout successful",
-        success: true,
-    }
-)
+    const response = NextResponse.json({
+      message: "Logout successful",
+      success: true,
+    });
 
     // Clear the token cookie
     response.cookies.set("token", "", {
       httpOnly: true,
       maxAge: 0, // Expire the cookie immediately
     });
-
+    return response;
   } catch (error: any) {
     return NextResponse.json(
       { error: error.message || "Internal Server Error" },
