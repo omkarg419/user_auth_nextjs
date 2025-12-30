5bff9ffc150abc9ff71c7779548d1d9a16104a32
omkarg419 <68603231+omkarg419@users.noreply.github.com>
2025-12-30 14:53:36 +0530

    feat: update middleware to handle public paths and redirect accordingly

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,9 +2,19 @@ import { NextResponse, NextRequest } from "next/server";
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-  return NextResponse.redirect(new URL("/home", request.url));
+  
+  const path = request.nextUrl.pathname;
+  const isPublicPath = path === '/login' || path === '/signup';
+  const token = request.cookies.get('token')?.value;
+
+  if(isPublicPath && token) {
+    return NextResponse.redirect(new URL('/', request.url));
 }
 
 export const config = {
-  matcher: "/about/:path*",
+  matcher: ['/',
+    '/profile',
+    '/login',
+    '/signup',
+  ]
 };
