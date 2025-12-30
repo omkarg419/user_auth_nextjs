aa0126038634815e1de617a56770bee866426fde
omkarg419 <68603231+omkarg419@users.noreply.github.com>
2025-12-30 14:56:13 +0530

    style: format middleware code for improved readability

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,19 +2,18 @@ import { NextResponse, NextRequest } from "next/server";
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-  
   const path = request.nextUrl.pathname;
-  const isPublicPath = path === '/login' || path === '/signup';
-  const token = request.cookies.get('token')?.value;
+  const isPublicPath = path === "/login" || path === "/signup";
+  const token = request.cookies.get("token")?.value;
 
-  if(isPublicPath && token) {
-    return NextResponse.redirect(new URL('/', request.url));
+  if (isPublicPath && token) {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+
+  if (!isPublicPath && !token) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
 }
-
 export const config = {
-  matcher: ['/',
-    '/profile',
-    '/login',
-    '/signup',
-  ]
+  matcher: ["/", "/profile", "/login", "/signup"],
 };
