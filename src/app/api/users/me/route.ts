3ae82eeb67a88e0f10ba6c875b15c95d53f478b3
omkarg419 <68603231+omkarg419@users.noreply.github.com>
2026-01-04 19:32:46 +0530

    feat: implement GET endpoint to retrieve user data by ID

---
+++ src/app/api/users/me/route.ts
@@ -0,0 +1,19 @@
+import { getDataFromToken } from "@/helpers/getDataFromToken";
+import { NextRequest, NextResponse } from "next/server";
+import User from "@/models/userModel";
+import { connectDB } from "@/dbconfig/dbconfig";
+
+connectDB();
+
+export async function GET(request: NextRequest) {
+  try {
+    const userId = await getDataFromToken(request);
+    const user = await User.findOne({ _id: userId }).select("-password ");
+    return NextResponse.json(
+      { data: user, message: "User found successfully" },
+      { status: 200 }
+    );
+  } catch (error: any) {
+    return NextResponse.json({ message: error.message }, { status: 500 });
+  }
+}
